59e012d229d399061c40703bb9e43d5cf2fc6ad7
Janvier-Irasubiza <61458240+Janvier-Irasubiza@users.noreply.github.com>
2025-04-21 21:29:14 +0200

    Add DataTables button extensions for enhanced table functionality

--- resources/views/layouts/apply.blade.php
+++ resources/views/layouts/apply.blade.php
@@ -577,6 +577,15 @@
     <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 
+    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
     <script>
         $('#toast').toast({ delay: 30000 });
         $('#toast').toast('show');
